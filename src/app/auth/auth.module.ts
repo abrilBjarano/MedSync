090ee090892657844ba3f65a3461b066c19884d3
abrilxcx <abrlbjrn@gmail.com>
2024-05-05 21:47:57 -0600

    Created Auth Module Pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
