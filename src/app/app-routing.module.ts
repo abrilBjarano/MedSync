9bbf6b740077a5686d7f8e36f5c7434a24d94887
abrilxcx <abrlbjrn@gmail.com>
2024-05-05 21:45:04 -0600

    Configured app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: 'meds',
+    loadChildren: () => import('./meds/meds.module').then( m => m.MedsModule )
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+    // si deja de funcionar, importar componente al app.module, porque no lo importé en ningún lado.
+  },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
