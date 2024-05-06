090374f2e51f4ad51c016f0152a9e875e5ced5fa
abrilxcx <abrlbjrn@gmail.com>
2024-05-05 20:30:13 -0600

    Created the 4 main Modules with their respective routing files

---
+++ src/app/meds/meds.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MedsRoutingModule } from './meds-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MedsRoutingModule
+  ]
+})
+export class MedsModule { }
