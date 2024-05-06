35714ce1e542dc15ea38b1395213d624edada4ff
abrilxcx <abrlbjrn@gmail.com>
2024-05-05 20:33:16 -0600

    Created Error404Page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Error404PageComponent
+  ],
   imports: [
     CommonModule
   ]
