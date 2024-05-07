c71ec2bb1f12b6e52809024468b832cfecf641b5
abrilxcx <abrlbjrn@gmail.com>
2024-05-07 16:33:52 -0600

    Created MedsModule main pages

--- src/app/meds/meds.module.ts
+++ src/app/meds/meds.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MedsRoutingModule } from './meds-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CreateEditPageComponent } from './pages/create-edit-page/create-edit-page.component';
+import { MedPageComponent } from './pages/med-page/med-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    HomePageComponent,
+    CreateEditPageComponent,
+    MedPageComponent
+  ],
   imports: [
     CommonModule,
     MedsRoutingModule
---
+++ src/app/meds/pages/create-edit-page/create-edit-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-page',
+  templateUrl: './create-edit-page.component.html',
+  styles: ``
+})
+export class CreateEditPageComponent {
+
+}
