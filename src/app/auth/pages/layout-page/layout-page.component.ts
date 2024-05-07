eb6764499c2a0a96e18f21f24276fd1b89152abf
abrilxcx <abrlbjrn@gmail.com>
2024-05-06 23:41:22 -0600

    Imported MaterialModule to AuthModule and created LoginPage template

--- src/app/auth/pages/layout-page/layout-page.component.ts
+++ src/app/auth/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-layout-page',
@@ -6,5 +7,4 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LayoutPageComponent {
-
 }
