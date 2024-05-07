9b26b478b368c3c6586d68cb91532a38f94d3809
abrilxcx <abrlbjrn@gmail.com>
2024-05-07 16:53:05 -0600

    Configured meds-routing and added router-outlet to layout-page

--- src/app/meds/meds-routing.module.ts
+++ src/app/meds/meds-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MedPageComponent } from './pages/med-page/med-page.component';
+import { CreateEditPageComponent } from './pages/create-edit-page/create-edit-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'create', component: CreateEditPageComponent },
+      { path: 'edit/:id', component: CreateEditPageComponent },
+      { path: 'home', component: HomePageComponent },
+      { path: ':id', component: MedPageComponent },
+      { path: '**', redirectTo: 'home' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
