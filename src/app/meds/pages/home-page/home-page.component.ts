06eff088934f5d4b600068d95373ddc3094eb777
abrilxcx <abrlbjrn@gmail.com>
2024-05-08 21:09:12 -0600

    Tested getHeroes() from meds.service

--- src/app/meds/pages/home-page/home-page.component.ts
+++ src/app/meds/pages/home-page/home-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MedsService } from '../../services/meds.service';
+import { Med } from '../../interfaces/med.interface';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styles: ``
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+
+  public meds: Med[] = [];
+
+  constructor( private medsService: MedsService ) {}
+
+  ngOnInit(): void {
+    this.medsService.getMeds()
+      .subscribe( meds => this.meds = meds);
+  }
 
 }
