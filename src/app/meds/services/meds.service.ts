cffee7c78a5c50f0d1f58a7cda0a41bda3d97dca
abrilxcx <abrlbjrn@gmail.com>
2024-05-08 20:31:18 -0600

    Created getMeds() in MedsService

--- src/app/meds/services/meds.service.ts
+++ src/app/meds/services/meds.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environments } from '../../../environments/environments.prod';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedsService {
 
+  private baseUrl = environments.baseUrl;
+
   constructor( private http: HttpClient ) { }
+
+
+  getMeds(): Observable<Med[]> {
+
+  }
 
 }
