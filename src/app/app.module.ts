1dce7b1aaffd26274643a6ef863fc12d0d1e7440
abrilxcx <abrlbjrn@gmail.com>
2024-05-08 21:05:48 -0600

    Imported HttpClientModule to app.module and updated meds.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    HttpClientModule,
     BrowserAnimationsModule,
   ],
   providers: [
--- src/app/meds/services/meds.service.ts
+++ src/app/meds/services/meds.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environments } from '../../../environments/environments';
 import { Observable } from 'rxjs';
 import { Med } from '../interfaces/med.interface';
+import { environments } from '../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedsService {
 
-  private baseUrl = environments.baseUrl;
+  private baseUrl: string = environments.baseUrl;
 
   constructor( private http: HttpClient ) { }
 
   getMeds(): Observable<Med[]> {
-    return this.http.get<Med[]>(`${ this.baseUrl}/meds`);
+    return this.http.get<Med[]>(`${ this.baseUrl }/meds`);
   }
 
 }
